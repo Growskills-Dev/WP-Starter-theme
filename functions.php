7b1082bcccf1623bf854a5bd4c073682a9110c71
Deny Celestino <denycelestino21@gmail.com>
2024-03-12 10:00:32 +0200

    changes

--- functions.php
+++ functions.php
@@ -225,9 +225,7 @@ function ajax_load_more() {
     $args = array(
         'post_type' => $post_type,
         'posts_per_page' => $posts_per_page,
-        //changes here
         'paged' => $paged,
-        //end changes here
         'order' => 'ASC',
         'meta_query' => array(
             array(
